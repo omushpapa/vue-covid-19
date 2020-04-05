481f00633ae34022bf39c2563241c0e67b56364e
Aswa Paul <aswapaul@gmail.com>
2020-04-05 12:22:12 +0300

    Add label Worlwide Cases to Total component
    Add totals into div

--- src/components/Totals.vue
+++ src/components/Totals.vue
@@ -1,64 +1,72 @@
 <template>
 <div class="container text-center h-100 d-flex align-items-center flex-column justify-content-around">
 
-  <ul class="list-unstyled">
-    <li class="media">
-      <img src="" class="mr-3" alt="">
-      <div class="media-body">
-        <h3 class="mt-0 mb-1">Total Confirmed</h3>
+  <div>
+    <ul class="list-group list-group-flush mb-4">
+      <li class="list-group-item">
+        <h5 class="text-secondary">Worldwide Cases</h5>
+      </li>
+    </ul>
 
-        <template v-if="loading">
-          <div class="pt-3">
-            <div class="spinner-border" role="status">
-              <span class="sr-only">Loading...</span>
+    <ul class="list-unstyled">
+      <li class="media">
+        <img src="" class="mr-3" alt="">
+        <div class="media-body">
+          <h3 class="mt-0 mb-1">Total Confirmed</h3>
+
+          <template v-if="loading">
+            <div class="pt-3">
+              <div class="spinner-border" role="status">
+                <span class="sr-only">Loading...</span>
+              </div>
             </div>
-          </div>
-        </template>
-        <template v-else="">
-          <h3 class="pt-3">{{ totalConfirmed | toLocaleString }}</h3>
-        </template>
+          </template>
+          <template v-else="">
+            <h3 class="pt-3">{{ totalConfirmed | toLocaleString }}</h3>
+          </template>
 
-      </div>
-    </li>
+        </div>
+      </li>
 
-    <li class="media my-4">
-      <img src="" class="mr-3" alt="">
-      <div class="media-body">
-        <h3 class="mt-0 mb-1">Total Recovered</h3>
-        
-        <template v-if="loading">
-          <div class="pt-3">
-            <div class="spinner-border" role="status">
-              <span class="sr-only">Loading...</span>
+      <li class="media my-4">
+        <img src="" class="mr-3" alt="">
+        <div class="media-body">
+          <h3 class="mt-0 mb-1">Total Recovered</h3>
+          
+          <template v-if="loading">
+            <div class="pt-3">
+              <div class="spinner-border" role="status">
+                <span class="sr-only">Loading...</span>
+              </div>
             </div>
-          </div>
-        </template>
-        <template v-else="">
-          <h3 class="pt-3">{{ totalRecovered | toLocaleString }}</h3>
-        </template>
+          </template>
+          <template v-else="">
+            <h3 class="pt-3">{{ totalRecovered | toLocaleString }}</h3>
+          </template>
 
-      </div>
-    </li>
+        </div>
+      </li>
 
-    <li class="media">
-      <img src="" class="mr-3" alt="">
-      <div class="media-body">
-        <h3 class="mt-0 mb-1">Total Deaths</h3>
-        
-        <template v-if="loading">
-          <div class="pt-3">
-            <div class="spinner-border" role="status">
-              <span class="sr-only">Loading...</span>
+      <li class="media">
+        <img src="" class="mr-3" alt="">
+        <div class="media-body">
+          <h3 class="mt-0 mb-1">Total Deaths</h3>
+          
+          <template v-if="loading">
+            <div class="pt-3">
+              <div class="spinner-border" role="status">
+                <span class="sr-only">Loading...</span>
+              </div>
             </div>
-          </div>
-        </template>
-        <template v-else="">
-          <h3 class="pt-3">{{ totalDeaths | toLocaleString }}</h3>
-        </template>
+          </template>
+          <template v-else="">
+            <h3 class="pt-3">{{ totalDeaths | toLocaleString }}</h3>
+          </template>
 
-      </div>
-    </li>
-  </ul>
+        </div>
+      </li>
+    </ul>
+  </div>
 
 </div>
 </template>
